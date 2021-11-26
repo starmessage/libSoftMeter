2b5f7bccad169b88bb35797c4822ec184dd7bf6f
starmessage <sales@starmessage.info>
2021-11-26 16:52:32 +0200

    improvements in c-sharp example of application analytics

--- samples/c-sharp/MainWindow.xaml.cs
+++ samples/c-sharp/MainWindow.xaml.cs
@@ -1,4 +1,10 @@
-﻿using System.Runtime.InteropServices;
+﻿//
+// file version: 1.1
+// (c) StarMessageSoftware
+//  https://www.starmessagesoftware.com/softmeter
+//
+
+using System.Runtime.InteropServices;
 using System.Windows;
 
 
@@ -18,14 +24,24 @@ namespace Demo_WpfApp_win
             string appName = "SoftMeter C-sharp DemoApp";
             string appVersion = "1.0";
             string appLicense = "pro";
-            string appEdition = "ms store"; // the edition uploaded to the Microsoft store
+            string appEdition = "Microsoft store"; // example: the edition uploaded to the Microsoft store
             string trackingID = "UA-1234-1"; // replace this with your own Google PropertyID
             SoftMeterDLL.enableLogfile(appName, "");
+
+            // if you has purchased a PRO subscription, add here the subscription details (instead of 1111111 and 123)
+            // This must be done before you call the start() function.
+            SoftMeterDLL.setOptions("subscriptionID=1111111");
+            SoftMeterDLL.setOptions("subscriptionType=123");
+
+            // call the start() function to initialize the softMeter library
             SoftMeterDLL.start(appName, appVersion, appLicense, appEdition, trackingID, UserGaveConsent);
 
+            // depending on the FREE/PRO edition, some hits might not be sent due to the limits of the FREE edition. 
+            // See more at: https://www.starmessagesoftware.com/softmeter/pro
             SoftMeterDLL.sendPageview("main","MainWindow");
             SoftMeterDLL.sendScreenview("MainWindow");
         }
+
 
         ~MainWindow()
         {
