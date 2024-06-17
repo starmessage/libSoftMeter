e956b8e98bf50c3f733992998fd1f9f9935fa8f3
starmessage <sales@starmessage.info>
2024-06-17 22:35:32 +0300

    Update SoftMeter-C-Api.h

--- SoftMeter-C-Api.h
+++ SoftMeter-C-Api.h
@@ -1,6 +1,6 @@
 //
 //  SoftMeter-C-Api.h
-//  File version: 1.3
+//  File version: 1.4
 //  Copyright StarMessage software. All rights reserved.
 //	https://www.starmessagesoftware.com/softmeter
 //
@@ -24,8 +24,7 @@
 // A: The "All-in-one" functions are defined in SoftMeter-C-Api-AIO.h
 
 
-// get the version of the library. 
-// ToDo: convert it to take the string in the parameters, getVersion(TCHAR* ver, int nMaxLen);
+// get the version of the library.
 EXPORT_API const TCHAR* CALL_CONV getVersion(void);
 
 // Enable or disable the log file. The default is disabled.
@@ -52,28 +51,20 @@ You must call this function before the start() function.
 1. Passing the SoftMeter PRO subscription details.
     setOptions("subscriptionID=1234567");
     setOptions("subscriptionType=2");
-	Where 1234567 should be replaced with your Google analytics account number,
-    i.e. the xxxxx part of UA-xxxxx-y and the subscriptionType must be 2.
+	Where 1234567 should be replaced with the number that we will give you after you buy your license.
     Alternatively, you can pass these two parameters packed in one function call E.g.
     setOptions("subscriptionID=1234567\nsubscriptionType=2\n");
     Note the \n character (aka, line feed, chr(10)) that separates the two parameters.
 
-2. Enabling collection of extra system info.
-   2.1 For GA4 properties (measurement_ID in the form of G-xxxxxx) 
-       The extra parameters are sent by default and only by SoftMeter PRO.
-   2.2. For Universal analytics properties (measurement_ID in the form of G-xxxxxx) 
-      setOptions("ExtraInfo=31")
-       Where the value (31) is a bit mask of extra pieces of information:
-       1 = When ON, use Screenviews. Otherwise use Pageviews to send this information.
-       2 = Total RAM memory
-       4 = Free RAM memory (The numbers are rounded so that they appear in the reporting in aggregated groups) 
-       8 = Number of CPU cores
-      16 = CPU model, e.g. Intel(R) Core(TM) i5-3470 CPU @ 3.20GHz
+2. Collection of extra system info.
+   For GA4 properties (measurement_ID in the form of G-xxxxxx)
+   The extra parameters are sent by default and only by SoftMeter PRO.
+   (Total RAM memory, Free RAM memory, Number of CPU cores, CPU model, etc)
  
-3. Passing the API secret for GA4 properties:
+3. Passing the API secret for GA4 properties (Mandatory):
     setOptions("ApiSecret=xxxxYYYYYzzzzIIII");
 
-4. Passing the Proxy parameters
+4. Passing the Proxy parameters (optional)
 	setOptions( "proxyAddress=192.1.127.123\n"
 				"proxyPort=808\n"
 				"proxyAuthScheme=2\n"
@@ -85,7 +76,7 @@ You must call this function before the start() function.
    0 = no authentication, 2 = NTLM, 4 = Passport, 8 = Digest, 16 = Negotiate
  If the proxy does not need authentication, do not pass the parameters authScheme, username and password
  
- 5. Enable the debug hits view of Google Analytics 4 (GA4-xxxxx)
+ 5. Enable the debug hits view of Google Analytics 4 (G-xxxxx)
     setOptions("debugHits=On");
  
 */
@@ -98,6 +89,7 @@ enum softMeterExtraInfo { eiUseScreenviews=1,  eiTotalRam=2, eiFreeRam=4, eiCPUc
 // Parameters:
 // - appLicense examples: free/trial/full/paid/etc
 // - appEdition examples: Apple store/Special build/Standard/IOS/Mac/Win/etc
+// - propertyID: your GA4 measurement ID, e.g. G-12345678
 // Must be called before any of the sendXXXX() functions
 // Returns true on success
 EXPORT_API  bool CALL_CONV start(const TCHAR *appName, const TCHAR *appVersion,
@@ -166,30 +158,6 @@ EXPORT_API  const TCHAR* CALL_CONV  getAnonymizedUserID(void);
 
 */
 
-    ///////////////////////////////////////////////////////////////////////
-	// __stdcall version of all the functions to be exported in the DLL
-    // Not needed for the MacOS platforms.
-    // These are the same function names but appended with _stdcall
-	// They do not need the EXPORT declaration  because they are exported by a .DEF file 
-    ///////////////////////////////////////////////////////////////////////
- 
-    /* Not needed any more (removed in v1.4.1)
-	const TCHAR*	__stdcall getVersion_stdcall(void);
-	const TCHAR*	__stdcall getLogFilename_stdcall(void);
-    void __stdcall enableLogfile_stdcall(const TCHAR *appName, const TCHAR *macBundleId);
-    void __stdcall disableLogfile_stdcall(void);
-    bool __stdcall setOptions_stdcall(const TCHAR *developerOptions);
-
-    bool __stdcall start_stdcall(const TCHAR *appName, const TCHAR *appVersion,
-		                        const TCHAR *appLicense, const TCHAR *appEdition,
-		                        const TCHAR *propertyID, const bool  userGaveConsent);
-    void __stdcall stop_stdcall(void);
-    bool __stdcall sendPageview_stdcall(const TCHAR *pagePath, const TCHAR *pageTitle);
-    bool __stdcall sendScreenview_stdcall(const TCHAR *screenName);
-    bool __stdcall sendEvent_stdcall(const TCHAR *eventAction, const TCHAR *eventLabel, const int eventValue);
-    bool __stdcall sendException_stdcall(const TCHAR *exceptionDescription, const bool isFatal);
-
-    */
 
     /////////////////////////////////////////////////////////////////////////
     // __cdeclspec version of all the functions to be exported in the DLL
